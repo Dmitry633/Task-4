f3f057e0aba3c504c89d01d56a61841dde12c09a
Dmitry <dmi_i88@mail.ru>
2019-11-03 21:40:46 +0300

    first commit task4

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//let money, time;											//объявляем переменный - делаем их глобальными, для их видимости вне функции
+'use strict';
+let money, time;											//объявляем переменный - делаем их глобальными, для их видимости вне функции
 function start() {
 	money = +prompt("Ваш бюджет на месяц?", '');
 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
@@ -18,74 +19,81 @@ let appData = {
 	optionalExpenses: {},
 	income: [],
 	timeData: time,
-	savings: true
-};
-
-function chooseExpenses(){
-	for (let i =0; i < 2; i++)	{						//ставим i < 2 т.к. у нас дважды задаются вопросы (см Задание 1)
-		let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-			b = prompt("Во сколько обойдется?", '');
-		
-			if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-			&& a != '' && b != '' && a.length < 50) {
-				console.log('done');
-				appData.expenses[a] = b;		// Точку здесь использовать нельзя, используются именно [] т.к. в "а" мы передаем переменную, 
-				// а если мы используем [], то "свойство" может содержать любую "строку"
-			} else {
-	i--
-			}
-	 }
-	
-}
-
- chooseExpenses();
-
- function detectDayBudget() {
-	appData.moneyPerDay = (appData.budget / 30).toFixed(); 
-
-	   alert('Ежедневный бюджет: ' + appData.moneyPerDay + "руб."); //ВНЕСЕНИЕ КОРРЕКТИРОВКИ, СОГЛАСНО ОТВЕТУ ПРЕПОДА
-}
-detectDayBudget();
- //appData.moneyPerDay = (appData.budget / 30).toFixed();					//перенесено в функцию detectDayBudget()
- /*Метод toFixed() округляет значение(в скобках - кол-во знаков после зпт. 
- Также он возвращает значение в виде строки*/
-//alert('Ежедневный бюджет: ' + appData.moneyPerDay); //после создания moneyPerDay можем вывести информацию в красивом виде
-/*Какой смысл + перед appData.moneyPerDay ?*/
-
-function detectLevel() {
-	if (appData.moneyPerDay < 100) {
-		console.log('Минимальный уровень достатка') ;
-	} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-		console.log('Средний уровень достатка') ;
-	} else if ( appData.moneyPerDay > 2000) {
-		console.log('Высокий уровень достатка') ;
-	
-	} else {
-		console.log ('Произошла ошибка')
-	}
-}
-detectLevel();
-
-function checkSavings(){
-	if(appData.savings == true){
-		let save = +prompt("Какова сумма накоплений?"),
-			percent = +prompt("Под какой процент?");
-
-		appData.monthIncome = save/100/12*percent;
-		alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
-	}
-}
-checkSavings();
-
-function chooseOptExpenses() {
-	for (let i = 1; i < 4; i++)	{						
-		let a = prompt("Статья необязательных расходов?");
-		
-			appData.optionalExpenses[i] = a;		// Точку здесь использовать нельзя, используются именно [] т.к. в "а" мы передаем переменную, 
-				// а если мы используем [], то "свойство" может содержать любую "строку"
-				console.log(appData.optionalExpenses);				//ВНЕСЕНИЕ КОРРЕКТИРОВКИ, СОГЛАСНО ОТВЕТУ ПРЕПОДА
+	savings: true,
+	chooseExpenses: function(){	/*добавляем методы в наш объект, а методы объекта - это функции. Методы создаются также как и свойства, 
+		но в значении этот свойства прописывается действие(функция) - в качкстве действий вырезаем функционал функций из Задания 3*/
+		for (let i =0; i < 2; i++)	{					
+			let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+				b = prompt("Во сколько обойдется?", '');
 			
-	 }
+				if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+				&& a != '' && b != '' && a.length < 50) {
+					console.log('done');
+					appData.expenses[a] = b;		
+			} else {
+				i--
+			}
+		}
+	},								//для использования этого метода нужно записать: addData.chooseExpenses()
+	detectDayBudget: function(){
+		appData.moneyPerDay = (appData.budget / 30).toFixed(); 
+		alert('Ежедневный бюджет: ' + appData.moneyPerDay + "руб.");
+	},
+	detectLevel: function(){
+		if (appData.moneyPerDay < 100) {
+			console.log('Минимальный уровень достатка') ;
+		} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+			console.log('Средний уровень достатка') ;
+		} else if ( appData.moneyPerDay > 2000) {
+			console.log('Высокий уровень достатка') ;
+		} else {
+			console.log ('Произошла ошибка')
+		}
+	},
+	checkSavings: function(){
+		if(appData.savings == true){
+			let save = +prompt("Какова сумма накоплений?"),
+				percent = +prompt("Под какой процент?");
+	
+			appData.monthIncome = save/100/12*percent;
+			alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+		}
 
-}
-chooseOptExpenses();
+	},
+	chooseOptExpenses: function(){
+		for (let i = 1; i < 4; i++)	{						
+			let a = prompt("Статья необязательных расходов?", "");
+			appData.optionalExpenses[i] = a;		
+			console.log(appData.optionalExpenses);			
+		}
+	},
+	chooseIncome: function() {//Создадим новый метод
+		let items = prompt('Что принесет дополнительный доход? (Перечислите через запятую)', '' );// - ответ получаем как строку
+
+		appData.income = items.split(', ');//запишем ответы пользователя - строку в массив income -для этого используем split
+
+		appData.income.push(prompt("Может что-то еще?"));
+		appData.income.sort();
+		for (let key of appData.income){
+
+			if (typeof(key) === 'string' && key != '' &&  key != null){
+				console.log(key)
+			}
+
+			else{
+				console.log("Произошла ошибка, введите корректные данные");
+				appData.chooseIncome();
+			}
+		}
+		appData.income.forEach(function(item, i){
+			alert("Способы доп. заработка: " );
+			alert(i+1 +  ': ' + item);
+
+		})
+	}
+};
+console.log( "Наша программа включает в себя данные: " );
+for (let key in appData){
+	console.log(key);
+	}
+
